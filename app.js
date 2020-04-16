ac01bc6e251f33011a53a10f09ef267385f04121
el-papi-chiludo <d7distrito@gmail.com>
2020-04-16 11:36:46 -0500

    c

--- app.js
+++ app.js
@@ -2,15 +2,28 @@
 const puppeteer = require('puppeteer');
 
 async function getPic() {
-  const browser = await puppeteer.launch({headless: true});
+  const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
   await page.goto('http://raboninco.com/HTC8');
   await page.waitFor(12000)
   //await page.screenshot({path: 'google.png'});
 
-  const page1 = await browser.newPage();
-  await page1.goto('http://raboninco.com/HTWJ');
-  await page1.waitFor(12000)
+  const page2 = await browser.newPage();
+  await page2.goto('http://raboninco.com/HTWJ');
+  await page2.waitFor(12000)
+
+  const page3 = await browser.newPage();
+  await page3.goto('http://raboninco.com/HUcj');
+  await page3.waitFor(12000)
+
+  const page4 = await browser.newPage();
+  await page4.goto('http://raboninco.com/HUdp');
+  await page4.waitFor(12000)
+
+  const page5 = await browser.newPage();
+  await page5.goto('http://raboninco.com/HUjq');
+  await page5.waitFor(12000)
+
   //await page1.screenshot({path: 'google.png'});
   await browser.close();
   console.log("ya se termino");
