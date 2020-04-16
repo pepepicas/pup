e43cd1d488dd9cdcb3b34e277c664bae0e2e7368
el-papi-chiludo <d7distrito@gmail.com>
2020-04-16 12:31:15 -0500

    as

--- app.js
+++ app.js
@@ -2,32 +2,37 @@
 const puppeteer = require('puppeteer');
 
 async function getPic() {
-  const browser = await puppeteer.launch({headless: false});
+  const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox']});
   const page = await browser.newPage();
   await page.goto('http://raboninco.com/HTC8');
   await page.waitFor(12000)
+  await page.close()
   //await page.screenshot({path: 'google.png'});
 
   const page2 = await browser.newPage();
   await page2.goto('http://raboninco.com/HTWJ');
-  await page2.waitFor(12000)
+  await page2.waitFor(10000)
+  await page2.close()
 
   const page3 = await browser.newPage();
   await page3.goto('http://raboninco.com/HUcj');
-  await page3.waitFor(12000)
+  await page3.waitFor(10000)
+  await page3.close()
 
   const page4 = await browser.newPage();
   await page4.goto('http://raboninco.com/HUdp');
-  await page4.waitFor(12000)
+  await page4.waitFor(10000)
+  await page4.close()
 
   const page5 = await browser.newPage();
   await page5.goto('http://raboninco.com/HUjq');
-  await page5.waitFor(12000)
+  await page5.waitFor(10000)
 
   //await page1.screenshot({path: 'google.png'});
   await browser.close();
   console.log("ya se termino");
   
 }
+
 
 getPic();
