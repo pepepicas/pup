1626b8f2ad240aa85d6b6f099dc2914ac0513dae
gilberto <d700distrito@gmail.com>
2020-04-17 17:28:00 -0500

    short link

--- app.js
+++ app.js
@@ -2,12 +2,12 @@
 const puppeteer = require('puppeteer');
 
 async function getPic() {
-  const browser = await puppeteer.launch({ args: ['--no-sandbox']});
+  const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
+  //adfly
   await page.goto('http://raboninco.com/HTC8');
   await page.waitFor(12000)
   await page.close()
-  //await page.screenshot({path: 'google.png'});
 
   const page2 = await browser.newPage();
   await page2.goto('http://raboninco.com/HTWJ');
@@ -27,6 +27,32 @@ async function getPic() {
   const page5 = await browser.newPage();
   await page5.goto('http://raboninco.com/HUjq');
   await page5.waitFor(10000)
+
+  //shorte.st
+  const page6 = await browser.newPage();
+  await page6.goto('http://gestyy.com/w9HlSE');
+  await page6.waitFor(12000)
+  await page6.close()
+
+  const page7 = await browser.newPage();
+  await page7.goto('http://gestyy.com/w9HlDc');
+  await page7.waitFor(10000)
+  await page7.close()
+
+  const page8 = await browser.newPage();
+  await page8.goto('http://gestyy.com/w9HlF3');
+  await page8.waitFor(10000)
+  await page8.close()
+
+  const page9 = await browser.newPage();
+  await page9.goto('http://gestyy.com/w9HlG6');
+  await page9.waitFor(10000)
+  await page9.close()
+
+  const page10 = await browser.newPage();
+  await page10.goto('http://gestyy.com/w9HlH9');
+  await page10.waitFor(10000)
+
 
   //await page1.screenshot({path: 'google.png'});
   await browser.close();
