bcf1f10250bbe8d88f04dc8076c282aa960e537a
gilberto <d700distrito@gmail.com>
2020-04-18 14:29:56 -0500

    previsualizacion de las capturas

--- app2.js
+++ app2.js
@@ -1,17 +1,21 @@
 const puppeteer = require("puppeteer");
 const fs = require("fs");
+const express = require('express')
+const app = express()
+
+app.use(express.static(__dirname + '/public'));
 
 let links = fs.readFileSync("links.txt", "utf-8");
 links = links.split(/\s+/);
 
-
 async function view() {
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch();
 
   for (let i = 0; i < links.length; i++) {
     const page = await browser.newPage();
     await page.goto(links[i]);
     console.log(`pagina actual: ${links[i]}`);
+    await page.screenshot({path: `${__dirname}/public/${i}.jpg`})
     await page.waitFor(15000);
     await page.close();
   }
@@ -20,6 +24,9 @@ async function view() {
   console.log("ya se termino");
 }
 
+app.listen(3000, () => {
+  console.log('server up in port 3000');
+})
 view();
 
 
